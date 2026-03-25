1a61e9e5da85a5fcc7188a756f4dbcd3ba73bc5c
erikmorenopino22-dot <erikmorenopino22@gmail.com>
2026-03-25 20:25:24 +0100

    Estructura Signed-off-by: erikmorenopino22-dot <erikmorenopino22@gmail.com>

---
+++ crud-ejercicio_Adicional/src/main/java/com/hibernate/model/Producto.java
@@ -0,0 +1,68 @@
+package com.hibernate.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "producto")
+public class Producto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
+	int codigo;
+	@Column(name = "nombre")
+	String nombre;
+	@Column(name = "stock")
+	int stock;
+	@Column(name = "precio")
+	int precio;
+
+	public Producto() {
+		super();
+	}
+
+	public Producto(String nombre, int stock, int precio) {
+		super();
+		this.nombre = nombre;
+		this.stock = stock;
+		this.precio = precio;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getStock() {
+		return stock;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+
+	public int getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(int precio) {
+		this.precio = precio;
+	}
+
+}
